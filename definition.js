24ea79e8e26381b9f881ac957415fbca2ed2f9f9
Miizzer <clement.jacquet.42600@gmail.com>
2024-09-16 11:23:14 +0200

    Creating 44 players
    Creation of the 44 players that will play in France Belgium England and Argentina

--- definition.js
+++ definition.js
@@ -1,8 +1,397 @@
-db.Players.insertOne({
-    lastname : "Doe",
-    firstname : "John",
-    birthday : "1998.07.12",
-    height : "182",
-    weight : "200",
-    position : "second striker"
-  })
+// Creating the players for France
+
+const giroud = db.Players.insertOne({
+  firstName: "Olivier",
+  lastName: "Giroud",
+  birthDate: "30.09.1986",
+  height : "193",
+  weight : "84",
+  position : "attaquant",
+});
+
+const mbappe = db.Players.insertOne({
+  firstName: "Kylian",
+  lastName: "Mbappe",
+  birthDate: "20.12.1998",
+  height : "178",
+  weight : "77",
+  position : "attaquant",
+});
+
+const griezmann = db.Players.insertOne({
+  firstName: "Antoine",
+  lastName: "Griezmann",
+  birthDate: "21.03.1991",
+  height : "175",
+  weight : "75",
+  position : "attaquant",
+});
+
+const matuidi = db.Players.insertOne({
+  firstName: "Blaise",
+  lastName: "Matuidi",
+  birthDate: "09.04.1987",
+  height : "180",
+  weight : "78",
+  position : "attaquant",
+});
+
+const pogba = db.Players.insertOne({
+  firstName: "Paul",
+  lastName: "Pogba",
+  birthDate: "15.03.1993",
+  height : "191",
+  weight : "84",
+  position : "milieu",
+});
+
+const kante = db.Players.insertOne({
+  firstName: "Ngolo",
+  lastName: "Kante",
+  birthDate: "29.03.1991",
+  height : "168",
+  weight : "73",
+  position : "milieu",
+});
+
+const varane = db.Players.insertOne({
+  firstName: "Raphael",
+  lastName: "Varane",
+  birthDate: "25.04.1993",
+  height : "191",
+  weight : "83",
+  position : "defenseur",
+});
+
+const umtiti = db.Players.insertOne({
+  firstName: "Samuel",
+  lastName: "Umtiti",
+  birthDate: "14.11.1993",
+  height : "182",
+  weight : "78",
+  position : "defenseur",
+});
+
+const hernandez = db.Players.insertOne({
+  firstName: "Lucas",
+  lastName: "Hernandez",
+  birthDate: "14.02.1996",
+  height : "184",
+  weight : "81",
+  position : "defenseur",
+});
+
+const pavard = db.Players.insertOne({
+  firstName: "Benjamin",
+  lastName: "Pavard",
+  birthDate: "28.03.1996",
+  height : "186",
+  weight : "83",
+  position : "defenseur",
+});
+
+
+// Creating the players for Belgium
+
+const courtois = db.Players.insertOne({
+  firstName: "Thibaut",
+  lastName: "Courtois",
+  birthDate: "11.05.1992",
+  height: "199",
+  weight: "96",
+  position: "gardien",
+});
+
+const alderweireld = db.Players.insertOne({
+  firstName: "Toby",
+  lastName: "Alderweireld",
+  birthDate: "02.03.1989",
+  height: "187",
+  weight: "91",
+  position: "defenseur",
+});
+
+const vertonghen = db.Players.insertOne({
+  firstName: "Jan",
+  lastName: "Vertonghen",
+  birthDate: "24.04.1987",
+  height: "189",
+  weight: "87",
+  position: "defenseur",
+});
+
+const meunier = db.Players.insertOne({
+  firstName: "Thomas",
+  lastName: "Meunier",
+  birthDate: "12.09.1991",
+  height: "191",
+  weight: "84",
+  position: "defenseur",
+});
+
+const witsel = db.Players.insertOne({
+  firstName: "Axel",
+  lastName: "Witsel",
+  birthDate: "12.01.1989",
+  height: "186",
+  weight: "81",
+  position: "milieu",
+});
+
+const debruyne = db.Players.insertOne({
+  firstName: "Kevin",
+  lastName: "De Bruyne",
+  birthDate: "28.06.1991",
+  height: "181",
+  weight: "70",
+  position: "milieu",
+});
+
+const tielemans = db.Players.insertOne({
+  firstName: "Youri",
+  lastName: "Tielemans",
+  birthDate: "07.05.1997",
+  height: "176",
+  weight: "72",
+  position: "milieu",
+});
+
+const hazard = db.Players.insertOne({
+  firstName: "Eden",
+  lastName: "Hazard",
+  birthDate: "07.01.1991",
+  height: "175",
+  weight: "74",
+  position: "attaquant",
+});
+
+const lukaku = db.Players.insertOne({
+  firstName: "Romelu",
+  lastName: "Lukaku",
+  birthDate: "13.05.1993",
+  height: "191",
+  weight: "94",
+  position: "attaquant",
+});
+
+const mertens = db.Players.insertOne({
+  firstName: "Dries",
+  lastName: "Mertens",
+  birthDate: "06.05.1987",
+  height: "169",
+  weight: "61",
+  position: "attaquant",
+});
+
+const carrasco = db.Players.insertOne({
+  firstName: "Yannick",
+  lastName: "Carrasco",
+  birthDate: "04.09.1993",
+  height: "181",
+  weight: "73",
+  position: "milieu",
+});
+
+
+// Creating the players for England
+
+const pickford = db.Players.insertOne({
+  firstName: "Jordan",
+  lastName: "Pickford",
+  birthDate: "07.03.1994",
+  height: "185",
+  weight: "77",
+  position: "gardien",
+});
+
+const walker = db.Players.insertOne({
+  firstName: "Kyle",
+  lastName: "Walker",
+  birthDate: "28.05.1990",
+  height: "183",
+  weight: "80",
+  position: "defenseur",
+});
+
+const stones = db.Players.insertOne({
+  firstName: "John",
+  lastName: "Stones",
+  birthDate: "28.05.1994",
+  height: "188",
+  weight: "77",
+  position: "defenseur",
+});
+
+const maguire = db.Players.insertOne({
+  firstName: "Harry",
+  lastName: "Maguire",
+  birthDate: "05.03.1993",
+  height: "194",
+  weight: "100",
+  position: "defenseur",
+});
+
+const shaw = db.Players.insertOne({
+  firstName: "Luke",
+  lastName: "Shaw",
+  birthDate: "12.07.1995",
+  height: "185",
+  weight: "75",
+  position: "defenseur",
+});
+
+const rice = db.Players.insertOne({
+  firstName: "Declan",
+  lastName: "Rice",
+  birthDate: "14.01.1999",
+  height: "185",
+  weight: "80",
+  position: "milieu",
+});
+
+const bellingham = db.Players.insertOne({
+  firstName: "Jude",
+  lastName: "Bellingham",
+  birthDate: "29.06.2003",
+  height: "186",
+  weight: "75",
+  position: "milieu",
+});
+
+const henderson = db.Players.insertOne({
+  firstName: "Jordan",
+  lastName: "Henderson",
+  birthDate: "17.06.1990",
+  height: "187",
+  weight: "80",
+  position: "milieu",
+});
+
+const sterling = db.Players.insertOne({
+  firstName: "Raheem",
+  lastName: "Sterling",
+  birthDate: "08.12.1994",
+  height: "170",
+  weight: "69",
+  position: "attaquant",
+});
+
+const kane = db.Players.insertOne({
+  firstName: "Harry",
+  lastName: "Kane",
+  birthDate: "28.07.1993",
+  height: "188",
+  weight: "86",
+  position: "attaquant",
+});
+
+const saka = db.Players.insertOne({
+  firstName: "Bukayo",
+  lastName: "Saka",
+  birthDate: "05.09.2001",
+  height: "178",
+  weight: "73",
+  position: "attaquant",
+});
+
+
+// Creating the players for Argentina
+
+const martinez = db.Players.insertOne({
+  firstName: "Emiliano",
+  lastName: "Martinez",
+  birthDate: "02.09.1992",
+  height: "195",
+  weight: "88",
+  position: "gardien",
+});
+
+const molina = db.Players.insertOne({
+  firstName: "Nahuel",
+  lastName: "Molina",
+  birthDate: "06.04.1998",
+  height: "175",
+  weight: "70",
+  position: "defenseur",
+});
+
+const romero = db.Players.insertOne({
+  firstName: "Cristian",
+  lastName: "Romero",
+  birthDate: "27.04.1998",
+  height: "185",
+  weight: "79",
+  position: "defenseur",
+});
+
+const otamendi = db.Players.insertOne({
+  firstName: "Nicolas",
+  lastName: "Otamendi",
+  birthDate: "12.02.1988",
+  height: "183",
+  weight: "81",
+  position: "defenseur",
+});
+
+const acuna = db.Players.insertOne({
+  firstName: "Marcos",
+  lastName: "Acuna",
+  birthDate: "28.10.1991",
+  height: "172",
+  weight: "72",
+  position: "defenseur",
+});
+
+const depaul = db.Players.insertOne({
+  firstName: "Rodrigo",
+  lastName: "De Paul",
+  birthDate: "24.05.1994",
+  height: "180",
+  weight: "70",
+  position: "milieu",
+});
+
+const paredes = db.Players.insertOne({
+  firstName: "Leandro",
+  lastName: "Paredes",
+  birthDate: "29.06.1994",
+  height: "180",
+  weight: "75",
+  position: "milieu",
+});
+
+const macallister = db.Players.insertOne({
+  firstName: "Alexis",
+  lastName: "Mac Allister",
+  birthDate: "24.12.1998",
+  height: "174",
+  weight: "72",
+  position: "milieu",
+});
+
+const messi = db.Players.insertOne({
+  firstName: "Lionel",
+  lastName: "Messi",
+  birthDate: "24.06.1987",
+  height: "170",
+  weight: "72",
+  position: "attaquant",
+});
+
+const alvarez = db.Players.insertOne({
+  firstName: "Julian",
+  lastName: "Alvarez",
+  birthDate: "31.01.2000",
+  height: "170",
+  weight: "71",
+  position: "attaquant",
+});
+
+const dimaria = db.Players.insertOne({
+  firstName: "Angel",
+  lastName: "Di Maria",
+  birthDate: "14.02.1988",
+  height: "180",
+  weight: "75",
+  position: "attaquant",
+});
